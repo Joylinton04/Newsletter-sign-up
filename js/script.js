912d1bedd930153330e99045fd20b8a87e31e5e1
JoyLinton04 <adujoy05@gmail.com>
2023-10-24 20:34:49 +0000

    match pattern refactor

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const emailInput = document.querySelector('#email')
 const errorMsg = document.querySelector('.error-msg')
 
 function active() {
-    if(emailInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{3}$/)) {
+    if(emailInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,3}$/)) {
         submitBtn.style.background = "linear-gradient(90deg,#ff527b,#ff6744)"
     } else {
         submitBtn.style.background = "hsl(235, 18%, 26%)"
@@ -12,7 +12,7 @@ function active() {
 
 const handleError = (e) => {
     e.preventDefault()
-    if(!emailInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{3}$/)) {
+    if(!emailInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,3}$/)) {
         emailInput.style.border = '1px solid red'
         emailInput.style.backgroundColor = '#f1c5c5'
         errorMsg.innerHTML = 'Valid email required'
