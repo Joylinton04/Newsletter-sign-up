8915100db5c907f9ebb6bb35f3a56086516fbea8
JoyLinton04 <adujoy05@gmail.com>
2023-10-24 17:53:43 +0000

    Final commit

---
+++ src/script.js
@@ -0,0 +1,28 @@
+const submitBtn = document.querySelector('#submit')
+const emailInput = document.querySelector('#email')
+const errorMsg = document.querySelector('.error-msg')
+
+
+const handleError = (e) => {
+    e.preventDefault()
+    if(!emailInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{3}$/)) {
+        emailInput.style.border = '1px solid red'
+        emailInput.style.backgroundColor = '#f1c5c5'
+        errorMsg.innerHTML = 'Valid email required'
+        return false
+    } else {
+        window.location.href = "success.html";
+    }
+    emailInput.style.backgroundColor = 'transparent'
+    emailInput.style.border = '1px solid #000'
+    errorMsg.innerHTML = ''
+}
+submitBtn.addEventListener('click', handleError)
+
+window.addEventListener('click', (e) => {
+    if(e.target.id !== 'submit') {
+        emailInput.style.backgroundColor = 'transparent'
+        emailInput.style.border = '1px solid #000'
+        errorMsg.innerHTML = ''
+    }
+})
